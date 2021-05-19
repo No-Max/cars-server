5ae9909296db72cd01a831c4dd6af016c0232787
No-Max <malyavko.max@gmail.com>
2021-05-19 15:14:35 +0300

    add endpoints for filters

--- db/cars.js
+++ db/cars.js
@@ -3,8 +3,8 @@ const cars = [
         id: 1,
         brand: 'Audi',
         model: 'A1',
-        img: 'audi-a1.jpeg',
-        bigImg: 'audi-a1-big.jpg',
+        img: 'audi-a1-1.jpeg',
+        bigImg: 'audi-a1-1-big.jpeg',
         price: 30000,
         description: 'Достойный авто по доступной цене. Приятный разгон и хорошая шумоизоляция.',
         parameters: [
@@ -13,13 +13,13 @@ const cars = [
                 value: '260 км/ч'
             }, {
                 name: 'Вес',
-                value: '1200кг'
+                value: '1250кг'
             }, {
                 name: 'Привод',
-                value: 'Полный'
+                value: 'Передний'
             }, {
                 name: 'Цвет',
-                value: 'Серый'
+                value: 'Синий'
             }, {
                 name: 'Модельный год',
                 value: '2020'
@@ -29,45 +29,45 @@ const cars = [
     {
         id: 2,
         brand: 'Audi',
-        model: 'A8',
-        img: 'audi-a8.jpeg',
-        bigImg: 'audi-a1-big.jpg',
-        price: 40000,
+        model: 'A1',
+        img: 'audi-a1-2.jpeg',
+        bigImg: 'audi-a1-2-big.jpeg',
+        price: 30000,
         description: 'Достойный авто по доступной цене. Приятный разгон и хорошая шумоизоляция.',
         parameters: [
             {
                 name: 'Максимальная скорость',
-                value: '260 км/ч'
+                value: '230 км/ч'
             }, {
                 name: 'Вес',
-                value: '1200кг'
+                value: '1100кг'
             }, {
                 name: 'Привод',
-                value: 'Полный'
+                value: 'Передний'
             }, {
                 name: 'Цвет',
                 value: 'Серый'
             }, {
                 name: 'Модельный год',
-                value: '2020'
+                value: '2019'
             }
         ],
     },
     {
         id: 3,
         brand: 'Audi',
-        model: 'Q7',
-        img: 'audi-q7.jpeg',
-        bigImg: 'audi-a1-big.jpg',
-        price: 60000,
+        model: 'A8',
+        img: 'audi-a8-1.jpeg',
+        bigImg: 'audi-a8-1-big.jpeg',
+        price: 40000,
         description: 'Достойный авто по доступной цене. Приятный разгон и хорошая шумоизоляция.',
         parameters: [
             {
                 name: 'Максимальная скорость',
-                value: '260 км/ч'
+                value: '300 км/ч'
             }, {
                 name: 'Вес',
-                value: '1200кг'
+                value: '1800кг'
             }, {
                 name: 'Привод',
                 value: 'Полный'
@@ -76,55 +76,55 @@ const cars = [
                 value: 'Серый'
             }, {
                 name: 'Модельный год',
-                value: '2020'
+                value: '2019'
             }
         ],
     },
     {
         id: 4,
-        brand: 'BMW',
-        model: 'e60',
-        img: 'bmw-e60.jpg',
-        bigImg: 'audi-a1-big.jpg',
-        price: 50000,
+        brand: 'Audi',
+        model: 'Q7',
+        img: 'audi-q7-1.jpeg',
+        bigImg: 'audi-q7-1-big.jpeg',
+        price: 60000,
         description: 'Достойный авто по доступной цене. Приятный разгон и хорошая шумоизоляция.',
         parameters: [
             {
                 name: 'Максимальная скорость',
-                value: '260 км/ч'
+                value: '280 км/ч'
             }, {
                 name: 'Вес',
-                value: '1200кг'
+                value: '2000кг'
             }, {
                 name: 'Привод',
                 value: 'Полный'
             }, {
                 name: 'Цвет',
-                value: 'Серый'
+                value: 'Серебристый'
             }, {
                 name: 'Модельный год',
-                value: '2020'
+                value: '2019'
             }
         ],
     },
     {
         id: 5,
         brand: 'BMW',
-        model: 'x5',
-        img: 'bmw-x5.jpg',
-        bigImg: 'audi-a1-big.jpg',
-        price: 40000,
+        model: 'e60',
+        img: 'bmw-e60-1.jpeg',
+        bigImg: 'bmw-e60-1-big.jpeg',
+        price: 50000,
         description: 'Достойный авто по доступной цене. Приятный разгон и хорошая шумоизоляция.',
         parameters: [
             {
                 name: 'Максимальная скорость',
-                value: '260 км/ч'
+                value: '360 км/ч'
             }, {
                 name: 'Вес',
-                value: '1200кг'
+                value: '1700кг'
             }, {
                 name: 'Привод',
-                value: 'Полный'
+                value: 'Задний'
             }, {
                 name: 'Цвет',
                 value: 'Серый'
@@ -136,25 +136,25 @@ const cars = [
     },
     {
         id: 6,
-        brand: 'Skoda',
-        model: 'Fabia',
-        img: 'skoda-fabia.jpeg',
-        bigImg: 'audi-a1-big.jpg',
+        brand: 'BMW',
+        model: 'x5',
+        img: 'bmw-x5-1.jpeg',
+        bigImg: 'bmw-x5-1-big.jpeg',
         price: 40000,
         description: 'Достойный авто по доступной цене. Приятный разгон и хорошая шумоизоляция.',
         parameters: [
             {
                 name: 'Максимальная скорость',
-                value: '260 км/ч'
+                value: '300 км/ч'
             }, {
                 name: 'Вес',
-                value: '1200кг'
+                value: '2100кг'
             }, {
                 name: 'Привод',
                 value: 'Полный'
             }, {
                 name: 'Цвет',
-                value: 'Серый'
+                value: 'Черный'
             }, {
                 name: 'Модельный год',
                 value: '2020'
@@ -164,10 +164,64 @@ const cars = [
     {
         id: 7,
         brand: 'Skoda',
+        model: 'Fabia',
+        img: 'skoda-fabia-1.jpeg',
+        bigImg: 'skoda-fabia-1-big.jpeg',
+        price: 40000,
+        description: 'Достойный авто по доступной цене. Приятный разгон и хорошая шумоизоляция.',
+        parameters: [
+            {
+                name: 'Максимальная скорость',
+                value: '210 км/ч'
+            }, {
+                name: 'Вес',
+                value: '1000кг'
+            }, {
+                name: 'Привод',
+                value: 'Передний'
+            }, {
+                name: 'Цвет',
+                value: 'Красный'
+            }, {
+                name: 'Модельный год',
+                value: '2010'
+            }
+        ],
+    },
+    {
+        id: 8,
+        brand: 'Skoda',
         model: 'Octavia',
-        img: 'skoda-octavia.jpeg',
-        bigImg: 'audi-a1-big.jpg',
+        img: 'skoda-octavia-1.jpeg',
+        bigImg: 'skoda-octavia-1-big.jpeg',
         price: 30000,
+        description: 'Достойный авто по доступной цене. Приятный разгон и хорошая шумоизоляция.',
+        parameters: [
+            {
+                name: 'Максимальная скорость',
+                value: '230 км/ч'
+            }, {
+                name: 'Вес',
+                value: '1800кг'
+            }, {
+                name: 'Привод',
+                value: 'Передний'
+            }, {
+                name: 'Цвет',
+                value: 'Красный'
+            }, {
+                name: 'Модельный год',
+                value: '2018'
+            }
+        ],
+    },
+    {
+        id: 9,
+        brand: 'Mitsubishi',
+        model: 'Lancer',
+        img: 'mitsu-lancer-1.jpeg',
+        bigImg: 'mitsu-lancer-1-big.jpeg',
+        price: 45000,
         description: 'Достойный авто по доступной цене. Приятный разгон и хорошая шумоизоляция.',
         parameters: [
             {
@@ -175,7 +229,34 @@ const cars = [
                 value: '260 км/ч'
             }, {
                 name: 'Вес',
-                value: '1200кг'
+                value: '1600кг'
+            }, {
+                name: 'Привод',
+                value: 'Передний'
+            }, {
+                name: 'Цвет',
+                value: 'Белый'
+            }, {
+                name: 'Модельный год',
+                value: '2011'
+            }
+        ],
+    },
+    {
+        id: 10,
+        brand: 'Mitsubishi',
+        model: 'Pajero',
+        img: 'mitsu-pajero-1.jpeg',
+        bigImg: 'mitsu-pajero-1-big.jpeg',
+        price: 45000,
+        description: 'Достойный авто по доступной цене. Приятный разгон и хорошая шумоизоляция.',
+        parameters: [
+            {
+                name: 'Максимальная скорость',
+                value: '180 км/ч'
+            }, {
+                name: 'Вес',
+                value: '2200кг'
             }, {
                 name: 'Привод',
                 value: 'Полный'
@@ -184,15 +265,104 @@ const cars = [
                 value: 'Серый'
             }, {
                 name: 'Модельный год',
+                value: '2016'
+            }
+        ],
+    },
+    {
+        id: 11,
+        brand: 'Mitsubishi',
+        model: 'Pajero',
+        img: 'mitsu-pajero-1.jpeg',
+        bigImg: 'mitsu-pajero-1-big.jpeg',
+        price: 45000,
+        description: 'Достойный авто по доступной цене. Приятный разгон и хорошая шумоизоляция.',
+        parameters: [
+            {
+                name: 'Максимальная скорость',
+                value: '180 км/ч'
+            }, {
+                name: 'Вес',
+                value: '2200кг'
+            }, {
+                name: 'Привод',
+                value: 'Полный'
+            }, {
+                name: 'Цвет',
+                value: 'Серый'
+            }, {
+                name: 'Модельный год',
+                value: '2016'
+            }
+        ],
+    },
+    {
+        id: 12,
+        brand: 'Volvo',
+        model: 's60',
+        img: 'volvo-s60-1.jpeg',
+        bigImg: 'volvo-s60-1-big.jpeg',
+        price: 45000,
+        description: 'Достойный авто по доступной цене. Приятный разгон и хорошая шумоизоляция.',
+        parameters: [
+            {
+                name: 'Максимальная скорость',
+                value: '220 км/ч'
+            }, {
+                name: 'Вес',
+                value: '1900кг'
+            }, {
+                name: 'Привод',
+                value: 'Передний'
+            }, {
+                name: 'Цвет',
+                value: 'Вишневый'
+            }, {
+                name: 'Модельный год',
+                value: '2019'
+            }
+        ],
+    },
+    {
+        id: 13,
+        brand: 'Volvo',
+        model: 'xc90',
+        img: 'volvo-xc90-1.jpeg',
+        bigImg: 'volvo-xc90-1-big.jpeg',
+        price: 50000,
+        description: 'Достойный авто по доступной цене. Приятный разгон и хорошая шумоизоляция.',
+        parameters: [
+            {
+                name: 'Максимальная скорость',
+                value: '240 км/ч'
+            }, {
+                name: 'Вес',
+                value: '2000кг'
+            }, {
+                name: 'Привод',
+                value: 'Полный'
+            }, {
+                name: 'Цвет',
+                value: 'Черный'
+            }, {
+                name: 'Модельный год',
                 value: '2020'
             }
         ],
     },
 ];
 
-const brands = Array.from(new Set(cars.map(car => car.brand)));
+const getCars = () => cars;
+
+const getBrands = () => Array.from(new Set(cars.map(car => car.brand)));
+
+const getCar = (id) => cars.find(car => car.id === Number(id));
+
+const getModels = (brand) => Array.from(new Set(cars.filter(car => car.brand === brand).map(car => car.model)))
 
 module.exports = {
-    cars,
-    brands
+    getCars,
+    getBrands,
+    getCar,
+    getModels,
 }
--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const express = require('express');
-const app = express();
 const fs = require('fs');
 const cors = require('cors');
-app.use(cors());
+const db = require('./db/cars.js');
 
+const app = express();
 const port =  process.env.PORT || 3000;
+
+app.use(cors());
  
 app.get('/', (req, res) => {
   res.send(fs.readFileSync('./html/index.html', 'utf8'));
 });
 
 app.get('/cars', (req, res) => {
-  res.send(require('./db/cars.js').cars)
+  res.send(db.getCars())
+})
+
+app.get('/cars/:id', (req, res) => {
+  const carId = req.params.id;
+  const car = db.getCar(carId);
+  if (!car) {
+    res.status(404).send(`Авто с id "${req.params.id}" не найден`);
+  }
+  res.send(car);  
 })
 
 app.get('/brands', (req, res) => {
-  res.send(require('./db/cars.js').brands)
+  res.send(db.getBrands())
+})
+
+app.get('/models/:brand', (req, res) => {
+  const brand = req.params.brand;
+  const models = db.getModels(brand);
+  if (models.length === 0) {
+    res.status(404).send(`Моделей для бренда "${brand}" не найдено`)
+  }
+  res.send(models);
 })
  
 app.listen(port, () => {
