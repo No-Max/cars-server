93218d3a1f3452c8db5183b32a1001f1ffb1be18
No-Max <malyavko.max@gmail.com>
2021-05-28 21:39:31 +0300

    add cars endpoint query parameter

--- index.js
+++ index.js
@@ -1,43 +1,60 @@
-const express = require('express');
-const fs = require('fs');
-const cors = require('cors');
-const db = require('./db/cars.js');
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const db = require("./db/cars.js");
 
 const app = express();
-const port =  process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
- 
-app.get('/', (req, res) => {
-  res.send(fs.readFileSync('./html/index.html', 'utf8'));
+
+app.get("/", (req, res) => {
+  res.send(fs.readFileSync("./html/index.html", "utf8"));
 });
 
-app.get('/cars', (req, res) => {
-  res.send(db.getCars())
-})
+app.get("/cars", (req, res) => {
+  const cars = db.getCars();
+  const isModelProvided = req.query && req.query.model;
+  const isBrandProvided = req.query && req.query.brand;
 
-app.get('/cars/:id', (req, res) => {
+  res.send(
+    cars.filter(({ brand, model }) => {
+      if (!isBrandProvided && !isModelProvided) {
+        return true;
+      }
+      if (isBrandProvided && !isModelProvided) {
+        return brand === req.query.brand;
+      }
+      if (isBrandProvided && isModelProvided) {
+        return brand === req.query.brand && model === req.query.model;
+      }
+      return false;
+    })
+  );
+});
+
+app.get("/cars/:id", (req, res) => {
   const carId = req.params.id;
   const car = db.getCar(carId);
   if (!car) {
     res.status(404).send(`Авто с id "${req.params.id}" не найден`);
   }
-  res.send(car);  
-})
+  res.send(car);
+});
 
-app.get('/brands', (req, res) => {
-  res.send(db.getBrands())
-})
+app.get("/brands", (req, res) => {
+  res.send(db.getBrands());
+});
 
-app.get('/models/:brand', (req, res) => {
+app.get("/models/:brand", (req, res) => {
   const brand = req.params.brand;
   const models = db.getModels(brand);
   if (models.length === 0) {
-    res.status(404).send(`Моделей для бренда "${brand}" не найдено`)
+    res.status(404).send(`Моделей для бренда "${brand}" не найдено`);
   }
   res.send(models);
-})
- 
+});
+
 app.listen(port, () => {
   console.log(`Server started on ${port}`);
 });
