e22ec9903b3ce9c96c1d574fcfe96c5508fa54ee
Maksim Maliauka <maksim.maliauka@vocovo.com>
2021-05-31 17:39:16 +0300

    update endpoints to work with db

--- index.js
+++ index.js
@@ -1,79 +1,99 @@
 const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
-const db = require("./db/cars.js");
-const sequelize = require("./db")
+const bodyParser = require('body-parser');
 
+const sequelize = require("./db");
+const { response } = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send(fs.readFileSync("./html/index.html", "utf8"));
 });
 
-app.get("/cars", (req, res) => {
-  const cars = db.getCars();
-  const isModelProvided = req.query && req.query.model;
-  const isBrandProvided = req.query && req.query.brand;
-
-  res.send(
-    cars.filter(({ brand, model }) => {
-      if (!isBrandProvided && !isModelProvided) {
-        return true;
-      }
-      if (isBrandProvided && !isModelProvided) {
-        return brand === req.query.brand;
-      }
-      if (isBrandProvided && isModelProvided) {
-        return brand === req.query.brand && model === req.query.model;
-      }
-      return false;
-    })
-  );
+app.get("/brands", async (req, res) => {
+  res.send(await sequelize.models.Brand.findAll());
 });
 
-app.get("/cars/:id", (req, res) => {
+app.get("/models/:brand", async (req, res) => {
+  const brand = req.params.brand;
+  const models = await sequelize.models.Model.findAll({
+    where: {
+      BrandId: brand
+    }
+  })
+  res.send(models);
+});
+
+// cars
+app.get("/cars", async (req, res) => {
+  let where = {};
+  if(req.query && req.query.brand) where.BrandId = req.query.brand;
+  if(req.query && req.query.model) where.ModelId = req.query.model;
+
+  res.send(await sequelize.models.Car.findAll({
+    where,
+    include: [
+      {
+        model: sequelize.models.Brand,
+      },
+      {
+        model: sequelize.models.Model,
+        attributes: ['id', 'name'],
+      },
+      {
+        model: sequelize.models.Parameter,
+        attributes: ['id', 'name', 'value'],
+      },
+    ],
+    attributes: ['id', 'img', 'bigImg', 'description', 'price'],
+  }))
+})
+
+app.get("/cars/:id", async (req, res) => {
   const carId = req.params.id;
-  const car = db.getCar(carId);
+  const car = await sequelize.models.Car.findOne({
+    where: {
+      id: carId,
+    },
+    include: [
+      {
+        model: sequelize.models.Brand,
+      },
+      {
+        model: sequelize.models.Model,
+        attributes: ['id', 'name'],
+      },
+      {
+        model: sequelize.models.Parameter,
+        attributes: ['id', 'name', 'value'],
+      },
+    ],
+    attributes: ['id', 'img', 'bigImg', 'description', 'price'],
+  })
   if (!car) {
     res.status(404).send(`Авто с id "${req.params.id}" не найден`);
   }
   res.send(car);
 });
 
-app.get("/brands", (req, res) => {
-  res.send(db.getBrands());
-});
-
-app.get("/models/:brand", (req, res) => {
-  const brand = req.params.brand;
-  const models = db.getModels(brand);
-  if (models.length === 0) {
-    res.status(404).send(`Моделей для бренда "${brand}" не найдено`);
-  }
-  res.send(models);
-});
-
-app.get("/test", async (req, res) => {
-  res.send(await sequelize.models.Car.findAll({
-    include: [{
-      model: sequelize.models.Brand,
-    },
-    {
-      model: sequelize.models.Model,
-    },
-  ]
-  }))
+app.post("/cars", async (req, res) => {
+  sequelize.models.Car.create(req.body)
+    .then(response => res.send(response))
+    .catch(error => res.status(400).send({ status: 400, message: 'В запросе ошибка', error }));
 })
 
 app.listen(port, async () => {
   console.log(`Server started on ${port}`);
-  sequelize.sync({ alter: true });
+  sequelize.sync();
   try {
     await sequelize.authenticate();
-    console.log('DB connection has been established successfully.');
+    console.log('DB connection has been established successfully');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
